3a409be61749c5f590f989fae26ba10606276823
John Kimotho <john.kimotho@valuechainfactory.com>
2026-05-07 13:08:14 +0300

    updates

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -6,13 +6,8 @@
       <img src="/logo.svg" alt="" />
 
       <nav class="sm:flex items-center gap-4 hidden">
-        <NuxtLink
-          v-for="link in navLinks"
-          :key="link.to"
-          :to="link.to"
-          custom
-          v-slot="{ isExactActive, navigate }"
-        >
+        <!--Home-->
+        <NuxtLink to="/" custom v-slot="{ isExactActive, navigate }">
           <button
             @click="navigate"
             :class="
@@ -21,11 +16,108 @@
                 : 'text-[#1F2A2A] p-3 hover:text-primary-800'
             "
           >
-            {{ link.label }}
+            Home
+          </button>
+        </NuxtLink>
+
+        <!--About-->
+        <div class="relative" ref="dropdownRef" >
+          <button
+            @mouseenter="open"
+            class="flex gap-1.5 items-center py-3 px-7 text-[#1F2A2A] p-3 hover:text-primary-800"
+          >
+            About us
+
+            <svg
+              width="12"
+              height="7"
+              viewBox="0 0 12 7"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6 4.45487L10.6669 0L12 1.27256L6 7L0 1.27256L1.33312 0L6 4.45487Z"
+                fill="currentColor"
+              />
+            </svg>
+          </button>
+
+          <div
+            v-show="dropdownOpen"
+            class="absolute mt-2 w-40 bg-white shadow rounded p-2"
+          >
+          <ul class="list-none space-y-1.5">
+            <li>
+              <a href="#who_we_are" class="rounded-sm hover:bg-primary-700 p-2.5 text hover:text-white text-sm w-full! block">
+                Who we are
+              </a>
+            </li>
+             <li>
+              <a href="#strategy" class="rounded-sm hover:bg-primary-700 p-2.5 text hover:text-white text-sm w-full! block">
+              Our Strategy Focus
+              </a>
+            </li>
+             <li>
+              <a href="#programs" class="rounded-sm hover:bg-primary-700 p-2.5 text hover:text-white text-sm w-full! block">
+                Programme Areas
+              </a>
+            </li>
+             <li>
+              <a href="#flagship" class="rounded-sm hover:bg-primary-700 p-2.5 text hover:text-white text-sm w-full! block">
+                Flagship Initiatives
+              </a>
+            </li>
+             <li>
+              <a href="#impact" class="rounded-sm hover:bg-primary-700 p-2.5 text hover:text-white text-sm w-full! block">
+                Our Impact
+              </a>
+            </li>
+             <li>
+              <a href="#structure" class="rounded-sm hover:bg-primary-700 p-2.5 text hover:text-white text-sm w-full! block">
+               Our Structure
+              </a>
+            </li>
+
+            <li>
+              <a href="#team" class="rounded-sm hover:bg-primary-700 p-2.5 text hover:text-white text-sm w-full! block">
+              The Team
+              </a>
+            </li>
+
+          </ul>
+          </div>
+        </div>
+
+        <!--Blog-->
+        <NuxtLink to="/blog" custom v-slot="{ isExactActive, navigate }">
+          <button
+            @click="navigate"
+            :class="
+              isExactActive
+                ? 'py-3 px-7 border-b-2 border-primary text-primary font-semibold'
+                : 'text-[#1F2A2A] p-3 hover:text-primary-800'
+            "
+          >
+            Blog
+          </button>
+        </NuxtLink>
+
+        <!--Media-->
+        <NuxtLink to="/media" custom v-slot="{ isExactActive, navigate }">
+          <button
+            @click="navigate"
+            :class="
+              isExactActive
+                ? 'py-3 px-7 border-b-2 border-primary text-primary font-semibold'
+                : 'text-[#1F2A2A] p-3 hover:text-primary-800'
+            "
+          >
+            Media
           </button>
         </NuxtLink>
       </nav>
 
+      <!--Mobile Navbar-->
       <div class="flex gap-4 items-center">
         <div class="relative">
           <button @click="isOpen = !isOpen" class="block sm:hidden">
@@ -51,7 +143,8 @@
             class="h-screen fixed w-50 top-0 right-0 bg-white shadow-lg rounded-lg py-10 px-3 z-50 space-y-4"
           >
             <div class="w-full items-start justify-between flex">
-              <button @click="isOpen = false"
+              <button
+                @click="isOpen = false"
                 class="bg-primary hover:bg-primary-300 cursor-pointer rounded-full p-1"
               >
                 <svg
@@ -94,7 +187,9 @@
               </NuxtLink>
             </div>
 
-            <a href="#contact" class="secondary-btn w-fit ml-auto py-2!">Contact Us</a>
+            <a href="#contact" class="secondary-btn w-fit ml-auto py-2!"
+              >Contact Us</a
+            >
           </div>
         </div>
 
@@ -105,18 +200,42 @@
 </template>
 
 <script setup>
-import { ref, watch } from "vue";
+import { ref, watch, onMounted, onBeforeUnmount } from "vue";
 
 const isOpen = ref(false);
+const dropdownOpen = ref(false);
+let timeout = null;
 
 watch(isOpen, (val) => {
   document.body.style.overflow = val ? "hidden" : "";
 });
 
-const navLinks = [
-  { label: "Home", to: "/" },
-  { label: "About us", to: "/about" },
-  { label: "Blog", to: "/blog" },
-  { label: "Media", to: "/media" },
-];
+const open = () => {
+  clearTimeout(timeout);
+  dropdownOpen.value = true;
+};
+
+const close = () => {
+  timeout = setTimeout(() => {
+    dropdownOpen.value = false;
+  }, 150);
+};
+
+const dropdownRef = ref(null);
+
+const handleClickOutside = (event) => {
+  if (!dropdownRef.value) return;
+
+  if (!dropdownRef.value.contains(event.target)) {
+    close();
+  }
+};
+
+onMounted(() => {
+  document.addEventListener("mousedown", handleClickOutside);
+});
+
+onBeforeUnmount(() => {
+  document.removeEventListener("mousedown", handleClickOutside);
+});
 </script>
